c138accb9c9332f160c4bce6b3d86fc75a8ff280
Rihan Dhakal <rihandhakal10@gmail.com>
2025-12-30 21:38:29 +0545

    Time: 105 ms (74.95%) | Memory: 32 MB (96.10%) - LeetSync

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        seen = set()
+        left = 0
+        current_sum = 0
+        max_sum = 0
+
+        for right in range(len(nums)):
+           
+            while nums[right] in seen:
+                seen.remove(nums[left])
+                current_sum -= nums[left]
+                left += 1
+
+           
+            seen.add(nums[right])
+            current_sum += nums[right]
+
+            
+            if right - left + 1 == k:
+                max_sum = max(max_sum, current_sum)
+
+               
+                seen.remove(nums[left])
+                current_sum -= nums[left]
+                left += 1
+
+        return max_sum
